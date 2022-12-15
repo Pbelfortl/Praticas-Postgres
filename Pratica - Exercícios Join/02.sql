e727a5709e6f55a5699d9a60493a43c919506e8b
Pablo Belfort <pablobelfort@gmail.com>
2022-12-14 22:38:07 -0300

    03 done

---
+++ Pratica - Exercícios Join/02.sql
@@ -0,0 +1,1 @@
+SELECT testimonials.id, writer.name AS writer, recipient.name AS recipient, testimonials.message FROM testimonials JOIN users AS writer ON testimonials."writerId" = writer.id JOIN users AS recipient ON recipient.id = testimonials."recipientId";
---
+++ Pratica - Exercícios Join/03.sql
@@ -0,0 +1,1 @@
+SELECT educations."userId" AS id, users.name, courses.name AS course, schools.name AS school, educations."endDate" FROM educations JOIN users ON users.id = educations."userId" JOIN courses ON courses.id = educations."courseId" JOIN schools ON schools.id = educations."schoolId" WHERE educations."userId" = 30;
